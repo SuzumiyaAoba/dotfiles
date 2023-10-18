3d144c31a44b6f2fb7bea1466386dbd5307ac153
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2023-10-18 23:58:26 +0900

    feat(emacs): add alias

--- .zsh/config/emacs.zsh
+++ .zsh/config/emacs.zsh
@@ -1,7 +1,9 @@
 ## emacs
 if [[ $(command -v emacs) ]]; then
-  alias emacs='emacsclient -nw -c -a ""'
-  alias killemacs='emacsclient -e "(kill-emacs)"'
-  alias e='emacs'
-  export EDITOR='emacsclient -nw -c -a ""'
+    alias emacs='emacsclient -nw -c -a ""'
+    alias gemacs='emacsclient -c -a "" &'
+    alias killemacs='emacsclient -e "(kill-emacs)"'
+    alias e='emacs'
+    alias ge='gemacs'
+    export EDITOR='emacsclient -nw -c -a ""'
 fi
