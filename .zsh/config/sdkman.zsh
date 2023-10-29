c18eac9882d80b65adf3d468407b1f5042a94114
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2023-10-29 22:02:24 +0900

    feat(zsh): modify sdkman config

--- .zsh/config/sdkman.zsh
+++ .zsh/config/sdkman.zsh
@@ -1,2 +1,10 @@
-export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+case ${OSTYPE} in
+    darwin*)
+        export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+    ;;
+    linux*)
+    # setting for linux
+    ;;
+esac
+
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
